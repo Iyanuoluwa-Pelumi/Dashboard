c2a26db90d46dd8fe912a7e0bcb0c8788ec5bfa8
Iyanuoluwa-Pelumi <ojoiyanuoluwa1@gmail.com>
2025-09-22 22:52:03 +0100

    sidebar

--- src/components/Dashboard/Sidebar.tsx
+++ src/components/Dashboard/Sidebar.tsx
@@ -14,26 +14,25 @@ export default function Sidebar() {
     <>
       {/* Mobile menu icon */}
       
-      <div>
-      <div className="md:hidden fixed top-0 left-0 w-screen mx-auto my-0 shadow h-15  bg-gray-900 flex justify-between items-center">
+      <div className="md:hidden fixed top-0 left-0 w-full shadow h-15 bg-gray-900 flex justify-between items-center">
+        
+        <Menu
+        className="w-8 h-8 text-white cursor-pointer"
+        onClick={() => setSidebar(!sidebar)}
+        />
 
-      <Menu className="w-8 h-8 text-white m-4 p-5 cursor-pointer"
-        onClick={() => setSidebar(!sidebar)}/>
-  
-
-      <Image
-        src='/Samantha.png'
+        <Image
+        src="/Samantha.png"
         alt="profile"
         width={32}
-          height={32}
-          className="rounded-full"
-        />
-        </div>   
+        height={32}
+        className="rounded-full"
+          />
+      
+
       </div>
 
-
-
-
+    
 
       {/* Sidebar */}
       <aside
@@ -50,16 +49,20 @@ export default function Sidebar() {
           <X className="w-8 h-8 text-white cursor-pointer" />
         </button>
 
-        <p className="absolute flex justify-center items-center right-21 top-14 md:right-22 md:top-14 lg:top-18 z-30 rounded-full w-6 h-6 text-[rgba(255,255,255,1)] bg-[rgba(220,52,52,1)]">4</p>
-
         {/* Profile section */}
         <div className="flex flex-col items-center gap-2 mt-10">
+        <div className="relative">
           <Image
-            src='/Samantha.png'
+            src="/Samantha.png"
             alt="profile"
             width={72}
             height={72}
           />
+            {/* Notification badge */}
+          <p className="absolute -top-2 -right-2 flex justify-center items-center w-6 h-6 rounded-full text-white text-xs bg-red-600">
+            4
+          </p>
+        </div>
           <h3 className="text-white font-bold text-lg">Somantha</h3>
           <small className="text-white opacity-50">Somantha@email.com</small>
         </div>
